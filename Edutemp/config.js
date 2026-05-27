1b08118dbb3eab324ffdac6210a00c8dd49cba57
Commander Q <263249418+CommandrQ@users.noreply.github.com>
2026-05-27 01:39:45 -0400

    Create config.js

---
+++ Edutemp/config.js
@@ -0,0 +1,5 @@
+/* ==========================================================================
+   CONFIG SECTOR // PRIVATE DATA NETWORK ARRAYS
+   ========================================================================== */
+const SUPABASE_URL = "https://dvyjupytbwbrcoyouxpf.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_NFV734_yORRlbG4NUOWVjQ_E_7BWnJ6";
